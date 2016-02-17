20bddce6345c8f9ff04169f6f48d18d7b790f6d4
Allard Stijnman <a.g.stijnman@gmail.com>
2016-02-17 14:24:34 +0100

    Add extra new relic logging

--- lily/wsgi.py
+++ lily/wsgi.py
@@ -14,10 +14,15 @@ framework.
 
 """
 import os
+
+from django.core.wsgi import get_wsgi_application
+from newrelic.agent import WSGIApplicationWrapper
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lily.settings.settings")
 
 # # This application object is used by any WSGI server configured to use this
 # # file. This includes Django's development server, if the WSGI_APPLICATION
 # # setting points here.
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = WSGIApplicationWrapper(application)
